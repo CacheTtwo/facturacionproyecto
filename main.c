15bb8ffb89560be2739220c7218a3401d84dcd53
ElMensajerodeDios <120017004+ElMensajerodeDios@users.noreply.github.com>
2022-12-11 13:55:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/*
+  Integrantes del grupo (Grupo C - FPI):
+  Sergio Fuentes Moreno
+  Manuel Alejandro Torres Moreno
+  Víctor Nolasco Sánchez
+  Javier Yago Giménez
+*/
+
 #include <stdio.h>
 #include <string.h>
 #define LISTA 15
@@ -10,19 +18,17 @@ struct productos{
     int stock;
     int lleno;
 };
-
-float albaranes(struct productos listaproductos[15]);
+void albaranes(struct productos listaproductos[15]);
 void factura(struct productos listaproductos[15], struct productos productosiniciales[15], int *numalbaranes);
 void new_art(struct productos lista[LISTA]);
 void cambio(char palabra[MAX_DESC]);
 
 int main() {
     int ops, numalbaranes = 5;
-    float preciototal = 0;
     //Lista de productos
-    struct productos listaproductos[15] = {{1, "Martillo de carpintero con mango de fibra de vidrio", 4.88, 50,1}, {2, "Maza de goma", 8.22, 4,1}, {3, "Juego de llaves combinadas", 11.99, 10,1}, {4, "Llave inglesa ajustable con tornillo", 5.54, 12,1}, {5, "Juego de laves Allen", 6.95, 20,1}, {6, "Juego de llaves de vaso", 21.99, 10,1}, {7, "Juego de destornilladores de precisión", 18.46, 15,1}, {8, "Pinza pelacables", 5.99, 3,1}, {9, "Alicates universales", 5.63, 12,1},{10, "Polimetro digital de bolsillo", 18.3, 7,1}, {11, "Polimetro digital multifuncion", 32.00, 12,1}};
+    struct productos listaproductos[15] = {{1, "Martillo de carpintero con mango de fibra de vidrio", 4.88, 50,1}, {2, "Maza de goma", 8.22, 4,1}, {3, "Juego de llaves combinadas", 11.99, 10,1}, {4, "Llave inglesa ajustable con tornillo", 5.54, 12,1}, {5, "Juego de laves Allen", 6.95, 20,1}, {6, "Juego de llaves de vaso", 21.99, 10,1}, {7, "Juego de destornilladores de precision", 18.46, 15,1}, {8, "Pinza pelacables", 5.99, 3,1}, {9, "Alicates universales", 5.63, 12,1},{10, "Polimetro digital de bolsillo", 18.3, 7,1}, {11, "Polimetro digital multifuncion", 32.00, 12,1}};
     //Copia de la lista que utilizamos para realizar ciertas comprobaciones en la funcion 2
-    struct productos productosiniciales[15] = {{1, "Martillo de carpintero con mango de fibra de vidrio", 4.88, 50,1}, {2, "Maza de goma", 8.22, 4,1}, {3, "Juego de llaves combinadas", 11.99, 10,1}, {4, "Llave inglesa ajustable con tornillo", 5.54, 12,1}, {5, "Juego de laves Allen", 6.95, 20,1}, {6, "Juego de llaves de vaso", 21.99, 10,1}, {7, "Juego de destornilladores de precisión", 18.46, 15,1}, {8, "Pinza pelacables", 5.99, 3,1}, {9, "Alicates universales", 5.63, 12,1}, {10, "Polimetro digital de bolsillo", 18.3, 7,1}, {11, "Polimetro digital multifuncion", 32.00, 12,1}};
+    struct productos productosiniciales[15] = {{1, "Martillo de carpintero con mango de fibra de vidrio", 4.88, 50,1}, {2, "Maza de goma", 8.22, 4,1}, {3, "Juego de llaves combinadas", 11.99, 10,1}, {4, "Llave inglesa ajustable con tornillo", 5.54, 12,1}, {5, "Juego de laves Allen", 6.95, 20,1}, {6, "Juego de llaves de vaso", 21.99, 10,1}, {7, "Juego de destornilladores de precision", 18.46, 15,1}, {8, "Pinza pelacables", 5.99, 3,1}, {9, "Alicates universales", 5.63, 12,1}, {10, "Polimetro digital de bolsillo", 18.3, 7,1}, {11, "Polimetro digital multifuncion", 32.00, 12,1}};
 
     do {
         printf("\n1. Introduccion de albaran\n");
@@ -32,11 +38,11 @@ int main() {
         printf("Elija una opcion:\n");
         scanf("%d", &ops);
 
-        switch(ops) {
+        switch(ops) { //Menu
             case 1:
                 printf("1. Introduccion de albaran\n");
                 if (numalbaranes > 0) {
-                    preciototal += albaranes(listaproductos);
+                    albaranes(listaproductos);
                     numalbaranes--; //Reduce en 1 (inicialmente vale 5) el numero de albaranes que podemos crear cada vez que vamos a la opcion 1
                 } else printf("\nYa pediste 5 albaranes, no se admiten mas.\n\n");
                 break;
@@ -52,7 +58,7 @@ int main() {
                     printf("Stock: %d\n", listaproductos[i].stock);
                     printf("Precio: %.2f\n", listaproductos[i].precio);
                     printf("El numero del articulo es %d\n", listaproductos[i].numarticulo);
-                    printf("%d\n", listaproductos[i].lleno);
+                    printf("Lleno: %d\n", listaproductos[i].lleno);
                 }
                 break;
             case 4:
@@ -65,9 +71,9 @@ int main() {
     return 0;
 }
 
-float albaranes(struct productos listaproductos[15]) {
+void albaranes(struct productos listaproductos[15]) {
     int p;
-    float total;
+    float totalalbaran = 0;
 
     printf("Introduzca el numero de articulos que tiene el albaran:\n");
     scanf("%d", &p);
@@ -82,6 +88,7 @@ float albaranes(struct productos listaproductos[15]) {
             scanf("%d", &cantidad[i]);
             if((numarticulo[i] < 1) || (numarticulo[i] > 15) || (cantidad[i] <= 0)) printf("Ese articulo no existe o la cantidad es negativa, vuelva a introducir los datos.\n");
         } while ((numarticulo[i] < 1) || (numarticulo[i] > 15) || (cantidad[i] <= 0)); //Si el articulo introducido no es valido por alguna de estas razones se vuelven a pedir los datos de este
+
         for (int j = 0; j < i; ++j) {
             if (numarticulo[i] == numarticulo[j]) {
                 printf("Ese articulo ya esta en este albaran, no se puede repetir.\n");
@@ -95,82 +102,65 @@ float albaranes(struct productos listaproductos[15]) {
             if (numarticulo[i] == listaproductos[j].numarticulo) { //Buscamos el producto pedido en la lista de articulos
                 if (cantidad[i] > listaproductos[j].stock) cantidad[i] = listaproductos[j].stock; //Si la cantidad pedida es mayor que el stock solo damos el stock que haya
                 listaproductos[j].stock -= cantidad[i]; //Reducimos el stock del articulo segun la cantidad pedida
-                total += cantidad[i] * listaproductos[j].precio;
+
+                totalalbaran += cantidad[i] * listaproductos[j].precio;
             }
         }
     }
-    printf("Precio total: %.2f", total);
-    return total;
+    printf("Importe total del albaran: %.2f\n", totalalbaran);
 }
 
-void factura(struct productos listaproductos[15], struct productos produtosiniciales[15], int *numalbaranes) {
-    int sin_pedidos = 0, cantidad, desc_larga;
-    float preciototal = 0;
+void factura(struct productos listaproductos[15], struct productos productosiniciales[15], int *numalbaranes) {
+    int sin_pedidos = 0, cantidad;
+    float preciototal = 0, importe;
 
     for (int i = 0; i < 15; ++i) {
-        if (listaproductos[i].stock == produtosiniciales[i].stock) sin_pedidos++;
+        if (listaproductos[i].stock == productosiniciales[i].stock) sin_pedidos++;
     }
     if (sin_pedidos == 15) {
         printf("No hay albaranes. Para mostrar la factura introduce uno primero.\n");
         return;
     } //Mediante el for y el if comprobamos si todavia no se ha introducido ningun albaran, en ese caso pedimos que se introduzca uno antes de mostrar una factura
 
-    for (int i = 0; i < 15; ++i) {
-        if (listaproductos[i].stock != produtosiniciales[i].stock)
-            if (strlen(listaproductos[i].descripcion) > desc_larga)
-                desc_larga = strlen(listaproductos[i].descripcion);
-    }
-    /*
-    printf("----------------------------------------------------------------\n");
+    printf("----------------------------------------------------------------\n"); //Empezamos a imprimir la factura
     printf("\t\tFactura -- Ferreteria Albacete --\n");
     printf("----------------------------------------------------------------\n");
     printf("\t Descripcion \t Cantidad \t Precio  Importe\n");
-     */
-    printf("%*s  %10s  %10s  %10s\n", desc_larga, "DESCRIPTION", "QUANTITY", "PRICE", "AMOUNT"); //DISPLAY TABLE WITH DELIVERED ITEMS IN SCREEN
     for (int i = 0; i < 15; ++i) {
-        if (listaproductos[i].stock != produtosiniciales[i].stock) {
-            cantidad = produtosiniciales[i].stock - listaproductos[i].stock;
-            printf("%*s  %10d  %10.2f  %10.2f\n", desc_larga, listaproductos[i].descripcion, cantidad, listaproductos[i].precio, listaproductos[i].precio * cantidad);
-            preciototal += cantidad * listaproductos[i].precio;
-            produtosiniciales[i].stock = listaproductos[i].stock;
+        if (listaproductos[i].stock != productosiniciales[i].stock) { //Mediante esta sentencia hacemos que solo vayan a la factura los articulos cuyo stock se haya visto modificado
+            cantidad = productosiniciales[i].stock - listaproductos[i].stock; //Calculamos la cantidad pedida restando al stock inicial el actual (despues de hacer albaranes)
+            importe = listaproductos[i].precio * cantidad; //Calculamos el importe a pagar multiplicando el precio de los articulos pedidos por su cantidad
+            printf("%s \t        %d \t   %.2f    %.2f\n", listaproductos[i].descripcion, cantidad, listaproductos[i].precio, importe); //Vamos imprimiendo todos los datos de cada articulo
+            preciototal += importe; //Vamos calculando el precio total uno por uno segun van pasando los articulos y sumando el importe de cada uno de estos
+            productosiniciales[i].stock = listaproductos[i].stock; //Despues de pasar cada articulo a la factura reiniciamos el stock de estos
         }
     }
-    /*
     printf("----------------------------------------------------------------\n");
-    printf("                                      TOTAL:  %8.2f\n", preciototal);
-    printf("----------------------------------------------------------------\n");
-    */
-    printf("\n-> TOTAL: %.2f\n\n\n", preciototal);
-    *numalbaranes = 5;
+    printf("                                           TOTAL:  %.2f\n", preciototal);
+    printf("----------------------------------------------------------------\n"); //Terminamos de imprimir la factura
+
+    *numalbaranes = 5; //Reiniciamos tambien a 5 el numero de albaranes que se pueden pedir despues de hacer la factura
 }
 
-void new_art(struct productos lista[LISTA]) //La estructura para la funcion es simple, se usa
-// un guardado de datos a través de un for, ya que es la forma de guardar datos en
-// una matriz o vector, recordar que para matrices se utilizan dos, la estructura ayuda, pues
-// escribir cuatro o cinco vectores para recopilar los datos habrían hecho de este proyecto uno mucho
-// mas extenso y dificil de compilar, pues el programa tendria que acceder a puntos distintos y no relacionados
-// de la memoria. Tambien cabe mencionar que el sistema de recoleccion de la variable lleno y el numero de
-// descripcion son simplemente beneficios de usar un loop 'for', y que siempre tengan en cuenta que los vectores
-// y matrices empiezan en 0, por tanto para que tanto las variables de llenado como los n_art sean correctos
-// hay que escribir el i+1 numero que se busque guardar.
-{
-    int i, aux = 0;
+void new_art(struct productos lista[LISTA]) {
+    int i, aux=0;
+
     printf("Introduzca el nuevo articulo.\n");
-    for(i=0; i<LISTA && aux==0; i++) {
-        if(lista[i].lleno==0) {
+    for(i=0; i<LISTA && aux==0; i++) { //Con el for recorremos la lista de productos
+        if(lista[i].lleno==0) { //Si encuentra un hueco vacio (lleno = 0) pide los datos del nuevo objeto y lo guarda en la lista
             printf("Introduce los datos requeridos:\n Descripcion:");
             fflush(stdin);
             fgets(lista[i].descripcion, MAX_DESC, stdin);
             cambio(lista[i].descripcion);
             fflush(stdin);
-            printf("Stock:\n ");
-            scanf("%d", &lista[i].stock);
             printf("Precio:\n ");
             scanf("%f", &lista[i].precio);
-            lista[i].numarticulo=i+1;
+            printf("Stock:\n ");
+            scanf("%d", &lista[i].stock);
+            lista[i].numarticulo = i+1;
             lista[i].lleno = 1;
             break;
-        } else if (lista[14].lleno == 1) {
+        } else if (lista[14].lleno == 1) { //Si no encuentra huecos vacios imprime un mensaje de error
             printf("No hay espacio disponible\n");
             break;
         }
